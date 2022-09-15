9b8fcd764847a708233ea376cc2d32f26d48a056
e1 <e1@e1.com>
2022-09-15 21:17:02 +0300

    add SeminarTask

---
+++ SeminarTask01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Введите первое целое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+
+if ((numA*numA)==numB)
+{
+    Console.WriteLine($"Число {numB} равно квадрату {numA}");
+}
+else 
+{
+    Console.WriteLine($"Число {numB} НЕ равно квадрату {numA}");
+}
