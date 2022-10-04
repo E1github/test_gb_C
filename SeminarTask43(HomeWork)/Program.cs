ee6000a41cf2964974717eec13d2c5549b5cff3a
e1 <e1@e1.com>
2022-10-04 20:30:07 +0300

    add minor changes to HomeWork Task6

--- SeminarTask43(HomeWork)/Program.cs
+++ SeminarTask43(HomeWork)/Program.cs
@@ -20,11 +20,16 @@ Console.Write("Введите коэфициент k2: ");
 double k2 = Convert.ToInt32(Console.ReadLine());
 
  double x = GetX(b1, k1, b2, k2);
- double y = k1 * x + b1;
+ double y = GetY(x, b1, k1);
 
 Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} -> ({x}; {y})");
 
 double GetX(double bb1, double kk1, double bb2, double kk2)
 {
     return (bb1 - bb2) / (kk2 - kk1);
+}
+
+double GetY(double xx, double bb1, double kk1)
+{
+    return kk1 * xx + bb1;
 }
